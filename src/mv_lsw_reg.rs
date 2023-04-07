9406942bf187f6ab7726f56b1f0b2926a4f74712
David <126030@qq.com>
2023-04-07 16:07:31 +0800

    TCAM review

--- src/mv_lsw_reg.rs
+++ src/mv_lsw_reg.rs
@@ -275,15 +275,15 @@ bit_struct! {
         Data:u11,
     }
 
-    struct mv_6390_TCAMP_PAGE_Op(u16) {
+    struct mv_6390_TCAM_PAGEX_Op(u16) {
         TCAMBusy:u1,
         TCAMOp:u3,
-        TCAMPage:u2,
+        TCAMage:u2,
         Reserved:u2,
         TCAMEntry:u8,
     }
 
-    struct mv_6390_TCAMP_PAGE0_0x02(u16) {
+    struct mv_6390_TCAM_PAGE0_0x02(u16) {
         Mask7_6:u2,
         Reserved:u3,
         Mask2_0:u3,
@@ -292,135 +292,136 @@ bit_struct! {
         SPV10_8:u3,
     }
 
-    struct mv_6390_TCAMP_PAGE0_0x03(u16) {
+    struct mv_6390_TCAM_PAGE0_0x03(u16) {
         Mask7_0:u8,
         SPV7_0:u8,
     }
 
-    struct mv_6390_TCAMP_PAGE0_0x04(u16) {
-        Mask:u8,
-        Reserved_PPPR:u4,
-        Reserved_PVID11_8:u4,
+    struct mv_6390_TCAM_PAGE0_0x04(u16) {
+        Mask_Reserved_PPRR:u4,
+        Mask_PVID11_8:u4,
+        Reserved_PPRI:u4,
+        PVID11_8:u4,
     }
 
-    struct mv_6390_TCAMP_PAGE0_0x05(u16) {
+    struct mv_6390_TCAM_PAGE0_0x05(u16) {
         Mask:u8,
         Index_PVID7_0:u8,
     }
 
-    struct mv_6390_TCAMP_PAGE0_0x06(u16) {
+    struct mv_6390_TCAM_PAGE0_0x06(u16) {
         Mask:u8,
         Octet1_49_DA1:u8,
     }
 
-    struct mv_6390_TCAMP_PAGE0_0x07(u16) {
+    struct mv_6390_TCAM_PAGE0_0x07(u16) {
         Mask:u8,
         Octet2_50_DA2:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x08(u16) {
+    struct mv_6390_TCAM_PAGE0_0x08(u16) {
         Mask:u8,
         Octet3_51_DA3:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x09(u16) {
+    struct mv_6390_TCAM_PAGE0_0x09(u16) {
         Mask:u8,
         Octet4_52_DA4:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x0A(u16) {
+    struct mv_6390_TCAM_PAGE0_0x0A(u16) {
         Mask:u8,
         Octet5_53_DA5:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x0B(u16) {
+    struct mv_6390_TCAM_PAGE0_0x0B(u16) {
         Mask:u8,
         Octet6_54_DA6:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x0C(u16) {
+    struct mv_6390_TCAM_PAGE0_0x0C(u16) {
         Mask:u8,
         Octet7_55_SA1:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x0D(u16) {
+    struct mv_6390_TCAM_PAGE0_0x0D(u16) {
         Mask:u8,
         Octet8_56_SA2:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x0E(u16) {
+    struct mv_6390_TCAM_PAGE0_0x0E(u16) {
         Mask:u8,
         Octet9_57_SA3:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x0F(u16) {
+    struct mv_6390_TCAM_PAGE0_0x0F(u16) {
         Mask:u8,
         Octet10_58_SA4:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x10(u16) {
+    struct mv_6390_TCAM_PAGE0_0x10(u16) {
         Mask:u8,
         Octet11_59_SA5:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x11(u16) {
+    struct mv_6390_TCAM_PAGE0_0x11(u16) {
         Mask:u8,
         Octet12_60_SA6:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x12(u16) {
+    struct mv_6390_TCAM_PAGE0_0x12(u16) {
         Mask:u8,
         Octet13_61_TAG1:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x13(u16) {
+    struct mv_6390_TCAM_PAGE0_0x13(u16) {
         Mask:u8,
         Octet14_62_TAG2:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x14(u16) {
+    struct mv_6390_TCAM_PAGE0_0x14(u16) {
         Mask:u8,
         Octet15_63_PRI_VID:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x15(u16) {
+    struct mv_6390_TCAM_PAGE0_0x15(u16) {
         Mask:u8,
         Octet16_64_VID:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x16(u16) {
+    struct mv_6390_TCAM_PAGE0_0x16(u16) {
         Mask:u8,
         Octet17_65_Type1:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x17(u16) {
+    struct mv_6390_TCAM_PAGE0_0x17(u16) {
         Mask:u8,
         Octet18_66_Type2:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x18(u16) {
+    struct mv_6390_TCAM_PAGE0_0x18(u16) {
         Mask:u8,
         Octet19_67_Data1:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x19(u16) {
+    struct mv_6390_TCAM_PAGE0_0x19(u16) {
         Mask:u8,
         Octet20_68_Data2:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x1A(u16) {
+    struct mv_6390_TCAM_PAGE0_0x1A(u16) {
         Mask:u8,
         Octet21_69_Data3:u8,
     }
-    struct mv_6390_TCAMP_PAGE0_0x1B(u16) {
+    struct mv_6390_TCAM_PAGE0_0x1B(u16) {
         Mask:u8,
         Octet22_70_Data4:u8,
     }
-    struct mv_6390_TCAMP_PAGE1_Data(u16) {
+    struct mv_6390_TCAM_PAGE1_Data(u16) {
         Mask:u8,
         Octet:u8,
     }
-    struct mv_6390_TCAMP_PAGE2_0x02(u16) {
+    struct mv_6390_TCAM_PAGE2_0x02(u16) {
         Continue:u1,
         Int:u1,
         IncTcamCtr:u1,
         VidOverride:u1,
         VIDData:u12,
     }
-    struct mv_6390_TCAMP_PAGE2_0x03(u16) {
+    struct mv_6390_TCAM_PAGE2_0x03(u16) {
         NextIndex_FlowID:u8,
         QPRI_Override:u1,
         QPRIData:u3,
         FPRIOverride:u1,
         FPRIData:u3,
     }
-    struct mv_6390_TCAMP_PAGE2_0x05(u16) {
+    struct mv_6390_TCAM_PAGE2_0x04(u16) {
         Reserved:u4,
         SourcePortFilter:u1,
         DPVData:u11,
     }
-    struct mv_6390_TCAMP_PAGE2_0x06(u16) {
+    struct mv_6390_TCAM_PAGE2_0x06(u16) {
         DPVMode:u2,
         ColorMode:u2,
         VTUPageOverride:u1,
@@ -429,39 +430,38 @@ bit_struct! {
         Reserved:u2,
         EgressActionPointer:u6,
     }
-    struct mv_6390_TCAMP_PAGE2_0x07(u16) {
+    struct mv_6390_TCAM_PAGE2_0x07(u16) {
         LoadBalanceOverride:u1,
         LoadBalanceData:u3,
         Reserved:u5,
         DSCPOverride:u1,
         DSCPData:u6,
     }
-
-    struct mv_6390_TCAMP_PAGE2_0x08(u16) {
+    struct mv_6390_TCAM_PAGE2_0x08(u16) {
         FActionOverride:u1,
         FActionData:u15,
     }
-
-    struct mv_6390_TCAMP_PAGE2_0x1C(u16) {
+    struct mv_6390_TCAM_PAGE2_0x1C(u16) {
         Reserved:u11,
         DebugPort:u5,
     }
-    struct mv_6390_TCAMP_PAGE2_0x1F(u16) {
+    struct mv_6390_TCAM_PAGE2_0x1F(u16) {
         HighHit:u8,
         LowHit:u8,
     }
-    struct mv_6390_TCAMP_PAGE3_Op(u16) {
+    struct mv_6390_TCAM_PAGE3_Op(u16) {
         TCAMBusy:u1,
         TCAMOp:u3,
-        TCAMPage:u2,
+        TCAMage:u2,
         Reserved:u2,
         AMEntry:u8,
     }
-    struct mv_6390_TCAMP_PAGE3_0x01(u16) {
+
+    struct mv_6390_TCAM_PAGE3_0x01(u16) {
         Reserved:u11,
         EgressPort:u5,
     }
-    struct mv_6390_TCAMP_PAGE3_0x02(u16) {
+    struct mv_6390_TCAM_PAGE3_0x02(u16) {
         Reserved:u1,
         FrameModeOverride:u1,
         FrameModeData:u2,
@@ -473,13 +473,13 @@ bit_struct! {
         Reserved3:u1,
         SAMode:u3,
     }
-    struct mv_6390_TCAMP_PAGE3_0x03(u16) {
+    struct mv_6390_TCAM_PAGE3_0x03(u16) {
         Reserved:u1,
         EgressVIDOverride:u1,
         EgressVIDMode:u2,
         EgressVIDData:u12,
     }
-    struct mv_6390_TCAMP_PAGE3_0x04(u16) {
+    struct mv_6390_TCAM_PAGE3_0x04(u16) {
         DSCPMode:u2,
         EgressDSCP:u6,
         Reserved:u1,
@@ -488,7 +488,7 @@ bit_struct! {
         EgressCFIData:u1,
         EgressFPRIData:u3,
     }
-    struct mv_6390_TCAMP_PAGE3_0x1F(u16) {
+    struct mv_6390_TCAM_PAGE3_0x1F(u16) {
         Reserved:u2,
         LastNon_ZeroHit:u6,
         Reserved1:u2,
